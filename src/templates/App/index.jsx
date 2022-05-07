1070b35bd4dbdde865843dd655cf23ca33e93a66
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-05-06 21:47:25 -0400

    feat: style button

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,9 +1,10 @@
+import StartScreen from '../../components/StartScreen';
 import * as Styled from './styles';
 function Home() {
   return (
     <div>
       <Styled.Wrapper background="gold">
-        <h1>Secret Word</h1>
+        <StartScreen />
       </Styled.Wrapper>
     </div>
   );
